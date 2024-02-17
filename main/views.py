53bd9261f52a7a56f6e315e59cfe6296d42c37d6
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2024-02-17 22:56:08 +0500

    17.02.2024
    22:50

--- main/views.py
+++ main/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .models import Credit, Question, Option
 
 
 def welcome(request):
+    credits = Credit.objects.all()
+    content = {
+        'credits': credits
+    }
+    return render(request, 'main/welcome.html', content)
+
+
+def credit_view(request):
+    if request.method == 'POST':
+        slug = request.POST.get('slug')
+        credit = Credit.objects.get(slug=slug)
+        content = {
+            'credits': credit,
+        }
+        return render(request, 'main/welcome.html', content)
+
     return render(request, 'main/welcome.html')
+
